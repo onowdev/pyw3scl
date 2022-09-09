137166e8b7ab87bbe1ef0d14ff4b036d3ebedc2f
onowdev <onowdev@gmail.com>
2022-09-09 10:22:11 +0700

    var&onelineVar

---
+++ 02_Variables/create_Variable.py
@@ -0,0 +1,67 @@
+x = 9
+y = "Agus"
+
+print(x)
+print(y)
+
+print("===================================")
+a = "Alpha"
+b = "Betha"
+c = "Charlie"
+d = "Delta"
+e = "Echo"
+f = "Finger"
+g = "Geronimo"
+h = "Hallowen"
+i = "India"
+j = "Java"
+k = "Kiril"
+l = "Lambo"
+m = "Marco"
+n = "Nano"
+o = "Omega"
+p = "Pizza"
+q = "Quaker"
+r = "Rambo"
+s = "Sally"
+t = "Tanggo"
+u = "Ulyana"
+v = "Violet"
+w = "Wardog"
+x = "Xena"
+y = "Yolo"
+z = "Zelda"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+print(o)
+print(p)
+print(q)
+print(r)
+print(s)
+print(t)
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+
+#print variable in one line
+
+a = "Python"
+b = "is"
+c = "Programing language"
+print(a,b,c)
