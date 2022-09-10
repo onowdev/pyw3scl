9dcbbce3515c93b53a11cfda67d5a1087c62f396
onowdev <onowdev@gmail.com>
2022-09-10 21:14:06 +0700

    create integr

--- 03_Number/typeofNum.py
+++ 03_Number/typeofNum.py
@@ -1,3 +1,6 @@
+from asyncio.unix_events import DefaultEventLoopPolicy
+
+
 a = 90
 b = 89.999
 c = 9j
@@ -5,3 +8,13 @@ c = 9j
 print(type(a))
 print(type(b))
 print(type(c))
+
+print("Create Integer")
+
+d = 90
+e = 809
+f = 789
+
+print(type(d))
+print(type(e))
+print(type(f))
