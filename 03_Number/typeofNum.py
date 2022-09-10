765fe23382a616ea0789e3e6337482f3494aaa51
onowdev <onowdev@gmail.com>
2022-09-10 21:42:40 +0700

    add E power of 10

--- 03_Number/typeofNum.py
+++ 03_Number/typeofNum.py
@@ -18,3 +18,16 @@ f = 789
 print(type(d))
 print(type(e))
 print(type(f))
+
+print("Create Floating point Number")
+
+g = 4.555
+h = 56.90
+i = 78.00
+k = 35e5
+l = 12E4
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(k))
+print(type(l))
