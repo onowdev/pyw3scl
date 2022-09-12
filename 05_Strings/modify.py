e2fe2d7e263b693af3236b1934dca66c4664503f
onowdev <onowdev@gmail.com>
2022-09-12 20:22:36 +0700

    modify String

---
+++ 05_Strings/modify.py
@@ -0,0 +1,22 @@
+"""
+Python - Modify Strings
+Python has a set of built-in methods that you can use on strings.
+
+"""
+
+# Upper Case
+a = "hello, world! my friend"
+print(a.upper())
+
+# LowerCase
+
+b = "HELLO_Python_InDONESIA"
+print(b.lower())
+
+# Remove Whitespace
+c = "Hello World on my Channel Python   "
+print(c.strip())
+
+# Replace String
+d = "Hello, world!"
+print(d.replace("e", "A"))
