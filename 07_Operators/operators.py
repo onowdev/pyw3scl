1beb6c01bc66a265901fe397fa96c4b651c98985
onowdev <onowdev@gmail.com>
2022-09-13 10:12:28 +0700

    aritmatic operators

---
+++ 07_Operators/operators.py
@@ -0,0 +1,27 @@
+"""
+Python Operators
+Operators are used to perform operations on variables and values.
+
+Python divides the operators in the following groups:
+
+    > Arithmetic operators
+    > Assignment operators
+    > Comparison operators
+    > Logical operators
+    > Identity operators
+    > Membership operators
+    > Bitwise operators
+
+"""
+
+print("Aritmatic Operator")
+
+a = 9
+b = 8
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a * b)
+print("Division", a / b)
+print("Modulus", a % b)
+print("Exponentiation", a ** 3)
+print("Floor division", a // b)
