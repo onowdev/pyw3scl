fb027c7c1c8aa27f79f06204ec3747cd10dc366f
onowdev <onowdev@gmail.com>
2022-09-14 08:57:17 +0700

    loop&If-list

--- 08_List/python_list.py
+++ 08_List/python_list.py
@@ -16,8 +16,22 @@ print(thisList)
 print("Access list items")
 print(thisList[1])
 print(thisList[0])
-
+print("==============")
 print("Change Value in list")
-thisList[1] = "Blackcurrant"
 
+thisList[1] = "Blackcurrant"
 print(thisList)
+
+print("==============")
+print("Loop with List")
+
+thisList1 = ["Apple", "Banana", "Cherry", "Duku", "Edamame"]
+for x in thisList1:
+    print(x)
+
+print("==============")
+print("Check if in List")
+thisList2 = ["apple","banana","cherry","duku"]
+if "banana" in thisList2:
+    print("Yes, 'apple' is in the fruits list")
+print("==============")
