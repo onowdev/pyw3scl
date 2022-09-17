f056c9eabf999b198a89b65a5e34a808d1bd3ce5
onowdev <onowdev@gmail.com>
2022-09-17 19:28:05 +0700

    Accessing Items

---
+++ 11_dictionaries/access_Items.py
@@ -0,0 +1,14 @@
+"""
+Accessing Items
+You can access the items of a dictionary 
+by referring to its key name, inside square brackets:
+"""
+print("Accessing Items")
+thisdict = {
+    "brand" : "Toyota",
+    "model" : "All New Land Cruiser",
+    "year" : "2022"
+}
+x = thisdict["model"]
+print(thisdict)
+print("the car is" , x)
