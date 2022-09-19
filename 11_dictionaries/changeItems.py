6219828791d300811d6847cd825699da0de8cac4
onowdev <onowdev@gmail.com>
2022-09-19 13:55:32 +0700

    change dictionary

---
+++ 11_dictionaries/changeItems.py
@@ -0,0 +1,12 @@
+import this
+
+
+print("Change Values in dictionary")
+thisdict1 = {
+    "brand" : "ford",
+    "model" : "mustang",
+    "year"  : 1960
+}
+thisdict1["year"] = 1980
+
+print(thisdict1)
