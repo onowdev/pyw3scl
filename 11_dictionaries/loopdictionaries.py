c99e54b9c9c7134004d40f99ecb7397431fbb3ef
onowdev <onowdev@gmail.com>
2022-09-20 10:56:56 +0700

    create loop in dict

---
+++ 11_dictionaries/loopdictionaries.py
@@ -0,0 +1,16 @@
+"""
+Loop Through a Dictionary
+You can loop through a dictionary by using a for loop.
+When looping through a dictionary, the return value are 
+the keys of the dictionary, but there are methods to return the values as well
+"""
+print("loop in dictionary")
+thisdict1 = {
+    "nama" : "Agus",
+    "usia" : "30",
+    "status" : "bapak anak 1"
+}
+print(thisdict1)
+
+for x in thisdict1:
+    print(x)
