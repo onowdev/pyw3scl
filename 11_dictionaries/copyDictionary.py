fc9f43a7fa0e2b2f3f0289ce21e67682ebb0f39f
onowdev <onowdev@gmail.com>
2022-09-20 11:18:35 +0700

    copy dictionary

---
+++ 11_dictionaries/copyDictionary.py
@@ -0,0 +1,13 @@
+"""
+Copy a Dictionary
+You cannot copy a dictionary simply by typing dict2 = dict1, 
+because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+"""
+thisdict = {
+    "brand" : "ford",
+    "model" : "Mustang",
+    "year" : 1965
+}
+mydict = thisdict.copy()
+print(mydict)
