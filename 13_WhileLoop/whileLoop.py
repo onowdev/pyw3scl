5066c415687392b403c31dcfd3ea43c145e88a13
onowdev <onowdev@gmail.com>
2022-09-24 22:02:16 +0700

    loops

--- 13_WhileLoop/whileLoop.py
+++ 13_WhileLoop/whileLoop.py
@@ -6,3 +6,12 @@ Python has two primitive loop commands:
 2. for loops
 
 """
+
+print("While Loops")
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+i = 7
+while i < 7:
+    print(i)
+    i +=7
