465cae423264529d333c67532aaa0ac8540180e1
onowdev <onowdev@gmail.com>
2022-09-25 21:14:46 +0700

    nested Loop

--- 14_for_loops/for_loops.py
+++ 14_for_loops/for_loops.py
@@ -54,3 +54,14 @@ for x in range(6):
     print(x)
 else:
     print("Finnally finished")
+
+print("Nested Loops")
+# A nested loop is a loop inside a loop.
+# The "inner loop" will be executed one time for each iteration of the "outer loop":
+
+adj = ["red","big","tasty"]
+fruits = ["apple","banana","cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
