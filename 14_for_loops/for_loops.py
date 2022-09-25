ad6462e4885ddf2c0742148610722260d8a460e8
onowdev <onowdev@gmail.com>
2022-09-25 19:56:03 +0700

    create Loops

--- 14_for_loops/for_loops.py
+++ 14_for_loops/for_loops.py
@@ -6,3 +6,7 @@ This is less like the for keyword in other programming languages,
 and works more like an iterator method as found in other object-orientated programming languages.
 With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
 """
+
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    print(x)
