f68f5daf46a04c02c3fc055cb3b0c5903ba18f99
onowdev <onowdev@gmail.com>
2022-09-26 18:34:12 +0700

    function with argument

--- 15_Function/create_func.py
+++ 15_Function/create_func.py
@@ -2,3 +2,21 @@ def myFunction():
     print("Hello this My Function")
 
 myFunction()
+
+print("Function With Argument")
+
+# Arguments
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+# The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+def function1(presiden):
+    print(presiden + "Kepala Nagara Indonesia")
+
+function1("Ir. Soekarno")
+function1("HM. Soeharto")
+function1("Bj. Habibie")
+function1("H. Abdurahman Wachid")
+function1("Ibu. Megawati soekarno Putri")
+function1("DR. Susilo Bambang Yudhoyono")
+function1("Ir. Joko Widodo")
