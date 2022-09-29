ac07ca3191dae6b180eba859fc0dea3ec292e8d3
onowdev <onowdev@gmail.com>
2022-09-29 10:19:53 +0700

    3 args

--- 16_Lamda/fungsiLamda.py
+++ 16_Lamda/fungsiLamda.py
@@ -12,5 +12,10 @@ print(x(9))
 
 # lamda multi Argument
 
-x = lambda a, b : a + B
+x = lambda a, b : a + b
 print(x(5, 6))
+
+# Summarize argument a, b, and c and return the result:
+
+x = lambda a, b, c : a + b + c
+print(x(4,5,6))
