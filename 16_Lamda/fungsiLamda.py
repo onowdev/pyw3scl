16a7674ee2ec82f580eb67d29914ca0c8648e979
onowdev <onowdev@gmail.com>
2022-09-29 10:17:50 +0700

    lambda two args

--- 16_Lamda/fungsiLamda.py
+++ 16_Lamda/fungsiLamda.py
@@ -4,5 +4,13 @@ A lambda function can take any number of arguments, but can only have one expres
 """
 # lamda arguments : expression
 
+from regex import B
+
+
 x = lambda a : a + 10
 print(x(9))
+
+# lamda multi Argument
+
+x = lambda a, b : a + B
+print(x(5, 6))
