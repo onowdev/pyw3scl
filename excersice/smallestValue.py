6920410174d1d9ba856c844893d0640fcc2f6cab
onowdev <onowdev@gmail.com>
2022-10-01 21:46:55 +0700

    salah kjetik

---
+++ excersice/smallestValue.py
@@ -0,0 +1,15 @@
+"""
+Below is code to find the smallest value 
+from a list of values. One line has an error 
+that will cause the code to not work as expected. 
+Which line is it?:
+"""
+
+smallest = None
+print("Before:", smallest)
+for itervar in [3,41,12,9,74,15]:
+    if smallest is None or itervar < smallest:
+        smallest = itervar
+        break
+    print("Loop:", itervar, smallest)
+print("Smallest:", smallest)
