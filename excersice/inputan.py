839955ebc49c6c2d9ce7c3d75594bdbeb492dea1
onowdev <onowdev@gmail.com>
2022-10-14 11:11:34 +0700

    tebak usia

--- excersice/inputan.py
+++ excersice/inputan.py
@@ -2,5 +2,11 @@ from email.policy import strict
 import string
 
 
-nilai = input("Masukan apa saja")
-print(nilai)
+# nilai = input("Masukan apa saja")
+# print(nilai)
+
+print ("V2")
+data = input('Berapa usia anda ?')
+usia = int(data)
+
+print("Sebentar lagi usia anda adalah", usia + 1)
